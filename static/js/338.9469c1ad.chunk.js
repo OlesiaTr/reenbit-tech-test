a6c4edb07adea029f06b5753219588449585f93b
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-03-15 18:22:50 +0000

    Deploying to gh-pages from @ OlesiaTr/reenbit-tech-test@8e690e27181a24b2a2132f53a1724ae02c8fbafd 🚀

---
+++ static/js/338.9469c1ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[338],{338:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{})}}}]);
+//# sourceMappingURL=338.9469c1ad.chunk.js.map
